dca1ceb1d67d4e6dba9b505e4b292ee1e6eeb127
朴 瑛姫 <X03025@tokyo.scientia.co.jp>
2026-06-03 14:04:19 +0900

    Add organization filter scrolling

--- i18n.js
+++ i18n.js
@@ -1,6 +1,6 @@
 const I18N_STORAGE_KEY = 'envPortalLang';
 const I18N_DEFAULT_LANG = 'ja';
-const APP_VERSION_FALLBACK = '2.1.33';
+const APP_VERSION_FALLBACK = '2.1.34';
 
 const I18N_MESSAGES = {
     ja: {
